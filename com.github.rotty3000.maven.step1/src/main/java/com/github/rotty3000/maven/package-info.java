55f6467cd570f829e3f2a9ad2cc6e98869277280
Raymond Auge <raymond.auge@liferay.com>
2019-05-02 10:51:32 -0400

    comments
    Signed-off-by: Raymond Auge <raymond.auge@liferay.com>

--- com.github.rotty3000.maven.step1/src/main/java/com/github/rotty3000/maven/package-info.java
+++ com.github.rotty3000.maven.step1/src/main/java/com/github/rotty3000/maven/package-info.java
@@ -17,5 +17,9 @@
  *  under the License.
  */
 
+
+/**
+ * This package uses an annotation provided by gogo.runtime which sais "I need GOGO installed!"
+ */
 @org.apache.felix.service.command.annotations.RequireGogo
 package com.github.rotty3000.maven;
